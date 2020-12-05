baa3403207944e1b8572114b9be34a9b1fa193f2
Luc Varoqui <luc@varoqui.org>
2020-12-05 12:00:39 +0100

    Remove boiler plate and add color guide

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,11 +4,16 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'rise-up-3',
+    title: 'Rise Up 3',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Association de création de comédie musicale inter-écoles & universités parisiennes.',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'navy-blue': '#131e47',
+        blue: '#99cacd',
+        sand: '#b38a58',
+        teal: '#107e7d',
+        coral: '#f0544f',
+      },
+    },
+  },
+}

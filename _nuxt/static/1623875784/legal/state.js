a29d3c2ae4d1d815228b17f1e840a2b522316f2e
lvaroqui <48593807+lvaroqui@users.noreply.github.com>
2021-06-16 20:36:25 +0000

    Deploying to gh-pages from @ lvaroqui/rise-up-3@5a0b844acfc8636e450098d6ab7d8da2bc65e9e2 🚀

---
+++ _nuxt/static/1623875784/legal/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Frise-up-3\u002F_nuxt\u002Fstatic\u002F1623875784",layout:"default",error:a,serverRendered:true,routePath:"\u002Flegal",config:{_app:{basePath:"\u002Frise-up-3\u002F",assetsPath:"\u002Frise-up-3\u002F_nuxt\u002F",cdnURL:a}}}}(null));

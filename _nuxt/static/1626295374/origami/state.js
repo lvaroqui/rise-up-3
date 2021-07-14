5e2b34862dc23b34b55da67731d77e136eb06a73
lvaroqui <48593807+lvaroqui@users.noreply.github.com>
2021-07-14 20:42:55 +0000

    Deploying to gh-pages from @ lvaroqui/rise-up-3@872242d70b2da5fa2d4b3ba7f450ffe85edde376 🚀

---
+++ _nuxt/static/1626295374/origami/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Frise-up-3\u002F_nuxt\u002Fstatic\u002F1626295374",layout:"default",error:a,serverRendered:true,routePath:"\u002Forigami",config:{_app:{basePath:"\u002Frise-up-3\u002F",assetsPath:"\u002Frise-up-3\u002F_nuxt\u002F",cdnURL:a}}}}(null));
